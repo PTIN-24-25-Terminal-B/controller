646686e617bd9a2cde12c463890b909d704ba519
Erich <voooigt@ItCrashesSometimes.home>
2025-03-23 18:52:46 +0100

    chore: added markdown documentation for the car model and updated .gitignore. fix: deleted the temporary database and added requirements.txt for the docker image

--- src/models/car_model.py
+++ src/models/car_model.py
@@ -1,7 +1,7 @@
 import json
 
 class carModel:
-    #variable types: carModel(integer, string, string, float, integer)
+    # variable types: carModel(integer, string, string, float, integer)
     def __init__(self, carType, seatCount, id=None, mileage=0.0, completedRuns=0):
         self.id = id
         self.seatCount = seatCount
@@ -10,26 +10,31 @@ class carModel:
         self.completedRuns = completedRuns
         return
     
+    # when reading the class as a string, returns de data in the model in json format
     def __str__(self):
         return json.dumps(self.__dict__, indent=4)
     
+    # adds a new completed run, including it's length in the car mileage
     def completeRun(self, miles=0.0):
         self.mileage+=miles
         self.completedRuns+=1
         return
     
-    def modifyCar(self, newType=None, newSeat=None, newId=None, newMileage=None, newRuns=None):
+    # allows to modify any of the stored data in the car model. Warning, changing it's id might create some errors
+    # like the target car not being found, or modifying the values of another car
+    def modifyCar(self, newId=None, newType=None, newSeat=None, newMileage=None, newRuns=None):
+        if newId is not None:
+            self.id = newId
         if newType is not None:
             self.carType = newType
         if newSeat is not None:
             self.seatCount = newSeat
-        if newId is not None:
-            self.id = newId
         if newMileage is not None:
             self.mileage = newMileage
         if newRuns is not None:
             self.completedRuns = newRuns
         return
     
+    # method to delete the car model
     def __del__(self):
         return
