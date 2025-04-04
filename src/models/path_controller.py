90fdb385ce50984dd0023838debc4bcb5fe369ec
Erich <voooigt@ItCrashesSometimes.home>
2025-04-04 20:14:45 +0200

    models.path_model: Added Point dataclass and path class

---
+++ src/models/path_controller.py
@@ -0,0 +1,30 @@
+from fastapi import HTTPException
+from dataclasses import dataclass, asdict
+from typing import List
+import json
+import redis
+
+@dataclass
+class Point:
+    x: float
+    y: float
+
+def toPoint(x: float,y: float):
+    return Point(x, y)
+
+class path :
+    def __init__(self, path: List[Point]):
+        self.coordList = path
+        return
+    
+    def __str__(self):
+        return json.dumps({
+            "points": [asdict(p) for p in self.points]
+        }, indent=4)
+
+    def __delattr__(self):
+        return
+    
+    def addPoint(self, newPoint: Point):
+        self.coordList.append(Point)
+        return path
