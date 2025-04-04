e1f5802fa4957462d5c81a523f3d7a56b4b9b304
Erich <voooigt@ItCrashesSometimes.home>
2025-04-04 20:28:15 +0200

    models/path_model: added modelId to path model
    modelId can be used to identify different paths.

--- src/models/path_model.py
+++ src/models/path_model.py
@@ -1,6 +1,7 @@
 from fastapi import HTTPException
 from dataclasses import dataclass, asdict
 from typing import List
+import string
 import json
 import redis
 
@@ -13,7 +14,8 @@ def toPoint(x: float,y: float):
     return Point(x, y)
 
 class path :
-    def __init__(self, path: List[Point]):
+    def __init__(self, pathId: string, path: List[Point]):
+        self.id = pathId
         self.coordList = path
         return
     
@@ -22,7 +24,7 @@ class path :
             "points": [asdict(p) for p in self.points]
         }, indent=4)
 
-    def __delattr__(self):
+    def __delattr__(self):       
         return
     
     def addPoint(self, newPoint: Point):
