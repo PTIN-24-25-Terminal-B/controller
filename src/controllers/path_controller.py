cf7989f463955a4374bd906ba4d25900dcb8156b
Erich <voooigt@ItCrashesSometimes.home>
2025-04-06 18:00:10 +0200

    /controller/path_controller.py: added a comment that reminds a value is temporary

--- src/controllers/path_controller.py
+++ src/controllers/path_controller.py
@@ -9,7 +9,7 @@ def create_path(x0: float, y0: float, x1:float, y1: float):
         for item in [x0, y0, x1, y1]:
             if not isinstance(item, float):
                 raise HTTPException(status_code=400, detail="input data has to be float")
-            if item > 100 or item < 0:
+            if item > 100 or item < 0: ##temporary coordinate max and min values
                 raise HTTPException(status_code=400, detail="coordinates do not fit in the map")
         
         newPath = Path("", [path_model.toPoint(x0, y0), path_model.toPoint(x1, y1)])
